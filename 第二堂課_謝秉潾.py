ba118feac565db67681e018510885d656baaefa5
wilbur88 <65432770+wilbur88@users.noreply.github.com>
2020-05-16 12:07:44 +0800

    第二堂課

---
+++ 第二堂課_謝秉潾.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 11:02:34 2020
+
+@author: domi
+"""
+
+
+
+
+
+
+
+score=input('輸入分數')
+score=int (score)
+
+if (score > 70):
+    print("及格")
+elif(score ==70):
+    print("再加油")
+else:
+    print("不及格")
+    
+
+
