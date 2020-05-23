1f2a6ed93593b49505e997733780848b46786318
wilbur88 <65432770+wilbur88@users.noreply.github.com>
2020-05-23 12:09:59 +0800

    Add files via upload

---
+++ 謝秉潾u3_.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 11:08:11 2020
+
+@author: domi
+"""
+
+print('x')
+print('x')
+print('x')
+while True:
+    print('y')
+    break
+
+i=0
+while i<3:
+    i+=1
+    print('z')
+
