f881e76fa8ec24099bb3342d911b060fefbd2722
wilbur88 <65432770+wilbur88@users.noreply.github.com>
2020-05-30 12:02:22 +0800

    Add files via upload

---
+++ U4_謝秉潾.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 11:24:53 2020
+
+@author: domi
+"""
+
+import turtle
+
+c=turtle.Screen()
+c.title('my window')
+c.bgcolor('pink')
+
+alex=turtle.Turtle()
+alex.color('black')
+alex.shape('circle')
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(100)
