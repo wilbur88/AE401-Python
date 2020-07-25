c9cf52c8a85b94c5279df3f81559b98b842c27d7
wilbur88 <65432770+wilbur88@users.noreply.github.com>
2020-07-25 11:53:49 +0800

    Add files via upload

---
+++ U10_2_謝秉潾.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 11:33:54 2020
+
+@author: domi
+"""
+
+
+letters='djcx,rfoxcm,d;lzkfl'
+print(letters[5])
+print(letters[-2])
+print(letters[1:6])
+print(letters[10:])
+
+letters='a duck goes into a bar'
+print(letters.capitalize())
+print(letters.title())
+print(letters.upper())
+print(letters.swapcase())
+
+string = 'this is string example'
+print(string.split())
+print(string.split('i'))
+print(string.split('i',1))
