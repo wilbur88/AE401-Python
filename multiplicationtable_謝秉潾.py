f0ae8fa7d43e8dde6f3ec7f0c5c8694043a4a936
wilbur88 <65432770+wilbur88@users.noreply.github.com>
2020-08-15 11:30:31 +0800

    Add files via upload

---
+++ multiplicationtable_謝秉潾.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 11:11:46 2020
+
+@author: domi
+"""
+
+
+import random
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"x",j,"=",i*j)
